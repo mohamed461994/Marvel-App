c4cb54fc837157cf5691b810a389637b71c69e71
MohamedSh <mohamed@MohamedShs-Mac.local>
2017-12-23 01:43:27 +0200

    creating first horizontal list of character in second view controller

---
+++ Marvel App/Marvel App/MarvelDetailesTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MarvelDetailesTableViewController.swift
+//  Marvel App
+//
+//  Created by MohamedSh on 12/22/17.
+//  Copyright © 2017 MohamedSh. All rights reserved.
+//
+
+import UIKit
+
+class MarvelDetailesTableViewController: UITableViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath) as! MarvelCollectionViewCell
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
