425f5def2aad6c4f1607c7bb83c860dd367e2d13
MohamedSh <mohamed@MohamedShs-Mac.local>
2017-12-23 11:11:22 +0200

    second view controller UI done

--- Marvel App/Marvel App/MarvelDetailesTableViewController.swift
+++ Marvel App/Marvel App/MarvelDetailesTableViewController.swift
@@ -9,7 +9,20 @@
 import UIKit
 
 class MarvelDetailesTableViewController: UITableViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+    
+    @IBOutlet weak var comicsCollectionView: UICollectionView!
+    @IBOutlet weak var EventsCollectionView: UICollectionView!
+    @IBOutlet weak var seriesCollectionView: UICollectionView!
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        /// case if its comics CollectionView
+        if collectionView == comicsCollectionView {
+            return 3
+        }/// case if its events CollectionView
+        else if collectionView == EventsCollectionView{
+            return 2
+        }
+        /// case if its series CollectionView
         return 7
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
