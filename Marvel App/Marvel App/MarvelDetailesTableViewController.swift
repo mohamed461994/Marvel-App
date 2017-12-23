ce66c5d815e39b71d902440209d8208a3e8f28ab
MohamedSh <mohamed@MohamedShs-Mac.local>
2017-12-23 11:30:40 +0200

    update second view controller class for load data

--- Marvel App/Marvel App/MarvelDetailesTableViewController.swift
+++ Marvel App/Marvel App/MarvelDetailesTableViewController.swift
@@ -13,17 +13,21 @@ class MarvelDetailesTableViewController: UITableViewController,UICollectionViewD
     @IBOutlet weak var comicsCollectionView: UICollectionView!
     @IBOutlet weak var EventsCollectionView: UICollectionView!
     @IBOutlet weak var seriesCollectionView: UICollectionView!
-    
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblDescription: UILabel!
+    var comicsList:[String:String]?
+    var seriessList:[String:String]?
+    var eventsList:[String:String]?
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         /// case if its comics CollectionView
         if collectionView == comicsCollectionView {
-            return 3
+            return comicsList?.count ?? 0
         }/// case if its events CollectionView
         else if collectionView == EventsCollectionView{
-            return 2
+            return eventsList?.count ?? 0
         }
         /// case if its series CollectionView
-        return 7
+        return seriessList?.count ?? 0
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath) as! MarvelCollectionViewCell
